3a0c5d7df864687bfe150b7025616a86c3c1af49
Evheniya Monastyrna <ev.monastyrna@outlook.com>
2019-01-27 22:25:49 +0200

    updated tests, added checks for input parameters

--- board/forms.py
+++ board/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 from django.core.exceptions import ValidationError
 
@@ -5,13 +7,10 @@ from django.core.exceptions import ValidationError
 class GameInputForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         self.fields['player_count'].label = "Number of Players"
-        self.fields['square_count'].label = \
-            "Number of Squares on the board"
+        self.fields['square_count'].label = "Number of Squares on the board"
         self.fields['card_count'].label = "Number of Cards in the deck"
-        self.fields['colors'].label = \
-            "Sequence of characters on the board"
+        self.fields['colors'].label = "Sequence of characters on the board"
         self.fields['cards'].label = "Cards in the deck"
 
     player_count = forms.IntegerField(min_value=1, max_value=4)
@@ -20,14 +19,40 @@ class GameInputForm(forms.Form):
     colors = forms.CharField()
     cards = forms.CharField()
 
-    # check all inputs here
+    def clean_colors(self):
+        colors = self.cleaned_data.get('colors')
+        if not colors.isalpha():
+            raise forms.ValidationError(
+                'Sequence of characters on the board must has letters only!')
+        if len(colors) != self.cleaned_data.get('square_count'):
+            raise forms.ValidationError(
+                'Sequence of characters on the board must be equal \
+                number of squares on the board!')
+        return colors
 
-    def clean(self):
-        cleaned_data = super().clean()
-        player_count = cleaned_data.get('player_count')
-        square_count = cleaned_data.get('square_count')
-        card_count = cleaned_data.get('card_count')
-        colors = cleaned_data.get('colors')
-        cards = cleaned_data.get('cards')
-        # if not player_count and not player_count:
-        #     raise forms.ValidationError('You have to write something!')
+    def clean_cards(self):
+        cards = self.cleaned_data.get('cards')
+        card_count = self.cleaned_data.get('card_count')
+        cards_split = cards.upper().split(",")
+
+        if len(cards_split) != card_count:
+            raise forms.ValidationError(
+                'Sequence of characters on the board must be equal \
+                number of cards in the deck!')
+
+        for card in cards_split:
+            if not card.isalpha():
+                # all cards in the deck must be letter value
+                raise forms.ValidationError(
+                    'Cards in the deck must have letters only!')
+
+            if len(card) > 2:
+                # all cards in the deck must have no more than 2 characters
+                raise forms.ValidationError(
+                    'The card must has less or equal 2 characters!')
+
+            if len(card) == 2 and re.search(r'^(.)\1$', card) == None:
+                # raise error, if card in the deck has 2 different characters
+                raise forms.ValidationError(
+                    'If the card has two characters, they must be the same!')
+        return cards
--- tests/board/test_game.py
+++ tests/board/test_game.py
@@ -2,7 +2,6 @@ import requests
 import pytest
 
 from django.urls import reverse
-from django.utils.translation import activate
 
 from board.forms import GameInputForm
 
@@ -31,11 +30,21 @@ class TestGame():
     cards, validity',
     [
         (2, 9, 4, 'AVHFISKSH', 'A,VV,F,S', True),
+        (2, 9, 4, 'AVHFISKSH', 'A,VB,F,S', False),
         (5, 9, 4, 'AVHFISKSH', 'A,VV,F,S', False),
         (4, 99, 4, 'AVHFISKSH', 'A,VV,F,S', False),
-        (4, 79, 201, 'AVHFISKSH', 'A,VV,F,S', False),
-        # (4, 5, 20, 'AVHFISKSH', 'A,VV,F,S', False),
-        # (4, 9, 5, 'AVHFISKSH', 'A,VV,F,S', False),
+        (4, 9, 201, 'AVHFISKSH', 'A,VV,F,S', False),
+        (4, 9, 4, 'FISKSH', 'A,VV,F,S', False),
+        (4, 9, 4, 'AVHFISKSH', 'A, V,F,S', False),
+        (4, 9, 4, 'AVHFISKSH', '9,7,34,6', False),
+        (4, 9, 4, 'AVHFISKSH', 'A,VV,6,S', False),
+        ('test', 'test', 'test', 'AVHFISKSH', 'A,VV,H,S', False),
+        ('test', 'test', 'test', '7', 'A,VV,H,S', False),
+        ('', '', '', '', '', False),
+        (-1, 9, 4, 'AVHFISKSH', 'A,VV,F,S', False),
+        (0, 9, 4, 'AVHFISKSH', 'A,VV,F,S', False),
+        (1, -9, 4, 'AVHFISKSH', 'A,VV,F,S', False),
+        (1, 9, -4, 'AVHFISKSH', 'A,VV,F,S', False),
     ]
 )
 def test_form_is_valid(player_count, square_count, card_count, colors, cards, validity):
